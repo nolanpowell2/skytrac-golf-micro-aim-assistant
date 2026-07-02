32f574a262c2cd6ad1558a0b28eed6784e741ab0
nolanpowell2 <nolanhpowell.2@gmail.com>
2026-07-02 12:45:33 -0500

    Fixed spelling errors

--- golf-launcher.bat
+++ golf-launcher.bat
@@ -2,8 +2,8 @@
 :: Start your Java program in a window named "MyKeyListener"
 start "MyKeyListener" java -jar "file path of GolfAim.jar"
 
-:: Start SkyTrac and wait for it to close
-start /wait "" "file path of your skytrac.exe file (usually under /Program Files)"
+:: Start SkyTrak and wait for it to close
+start /wait "" "file path of your skytrak.exe file (usually under /Program Files)"
 
 :: Force close ONLY the window titled "MyKeyListener"
 taskkill /fi "WINDOWTITLE eq MyKeyListener*" /f
